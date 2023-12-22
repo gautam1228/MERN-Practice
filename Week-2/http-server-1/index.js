33bd4c1434b6d2d2d19080e46dd7213914bb4478
gautam1228 <gautamsingh12122003@gmail.com>
2023-12-22 19:59:34 +0530

    Created an HTTP server running on a specified port.

--- Week-2/http-server-1/index.js
+++ Week-2/http-server-1/index.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+    console.log("Website was loaded...");
     res.send("Bum chiki bum bum..");
 })
 
 app.listen(port, () => {
     console.log('Example app listening on port ', port);
-}) 
+})
