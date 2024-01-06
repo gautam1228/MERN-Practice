a47fd5a109c5e3add502f3daecd04242ad89bf2b
gautam1228 <gautamsingh12122003@gmail.com>
2024-01-07 00:58:07 +0530

    Connected the backend with the database and created an http endpoint for creating new users.

--- Week-3/server_with_backend.js
+++ Week-3/server_with_backend.js
@@ -1,3 +1,53 @@
+const express = require("express");
 const mongoose = require("mongoose");
+const app = express();
+const port = 3000;
 
-mongoose.connect("mongodb+srv://gautamsingh12122003:RUrW6ihRmhU0k9TE@cluster0.h7cdvww.mongodb.net/");
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://gautamsingh12122003:RUrW6ihRmhU0k9TE@cluster0.h7cdvww.mongodb.net/user_app");
+
+const User = mongoose.model(
+    'users',
+    {
+        name : String,
+        username : String,
+        password : String
+    }
+);
+
+// const sample_user = new User({
+//     name : 'Gautam Singh',
+//     email : 'gautam123@gmail.com',
+//     password : '123456'
+// });
+
+app.post("/signin", async (req, res) => {
+    
+    const name = req.body.name;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const existing_User = await User.findOne({username : username});
+
+    if(existing_User){
+        return res.status(400).send("Username already exists.");
+    }
+
+    const new_user = new User({
+        name : name,
+        username : username,
+        password : password
+    });
+
+    new_user.save();
+
+    res.json({
+        msg : "User created successfully !"
+    })
+
+});
+
+app.listen(port, () => {
+    console.log('Server running on port : ' + port);
+})
