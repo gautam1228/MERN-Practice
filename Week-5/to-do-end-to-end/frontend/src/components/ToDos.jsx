ec4413832640d2eb69c0f14588bfe88032cb3971
gautam1228 <gautamsingh12122003@gmail.com>
2024-01-15 00:24:05 +0530

    End to end app created.

---
+++ Week-5/to-do-end-to-end/frontend/src/components/ToDos.jsx
@@ -0,0 +1,13 @@
+
+export function ToDos({todos}){
+    return <div>
+        {todos.map((todo)=>{
+            return <div>
+                <h1>{todo.title}</h1>
+                <h2>{todo.description}</h2>
+                <button>{todo.completed == true ? "Completed" : "Mark as Complete"}</button>
+            </div>
+        })}
+    </div>
+
+}
