2048fa8a95036bc90b373abdd11eb2bb61a1cdc6
gautam1228 <gautamsingh12122003@gmail.com>
2024-01-15 20:00:23 +0530

    Testing out some features.

---
+++ Week-6/useMemo-proj/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [num, setNum] = useState(0);
+  function SumCalc({num}){
+    num = parseInt(num);
+    return(
+      <div>
+        Sum is : {num*(num+1)/2}
+      </div>
+    )
+  }
+  return (
+    <>
+      <input type = "text" placeholder="number" onChange={(e) => {
+        setNum(e.target.value ? e.target.value : 0);
+      }}/>
+      <SumCalc num = {num}/>
+      <button onClick = {() => {
+        setCount(count+1);
+      }}>Counter {count}</button>
+    </>
+  )
+}
+
+export default App
