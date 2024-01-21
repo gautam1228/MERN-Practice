ac0169d56d08521373320c08e88badc08c08ea6a
gautam1228 <gautamsingh12122003@gmail.com>
2024-01-22 01:33:43 +0530

    Used the context API.

--- Week-7/Week-7-1/src/App.jsx
+++ Week-7/Week-7-1/src/App.jsx
@@ -1,45 +1,70 @@
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+// import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+// import { Landing } from '../src/components/Landing';
+// import { Dashboard } from '../src/components/Dashboard';
 
-import { Landing } from '../src/components/Landing';
-import { Dashboard } from '../src/components/Dashboard';
+import { useContext, useState } from "react"
+import { CountContext } from "./context";
+
 
 
 function App() {
   
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <BrowserRouter>
-        <TopBar/>
-        <Routes>
-          <Route path = '/' element = {<Landing/>}/>
-          <Route path = '/dashboard' element = {<Dashboard/>}/>
-        </Routes>
-      </BrowserRouter>
+      <CountContext.Provider value = {{count, setCount}}>
+        <CountRenderer/>
+        <Buttons />
+      </CountContext.Provider>
     </div>
   )
 }
 
-function TopBar(){
-
-  const navigate = useNavigate();
-
-  return (
-    <div>
-
-      <button onClick={()=>{
-        navigate('/');
-      }}>
-        Landing
-      </button>
-
-      <button onClick={()=>{
-        navigate('/dashboard');
-      }}>
-        Dashboard
-      </button>
-
-    </div>
-  )
+function CountRenderer(){
+  const { count } = useContext(CountContext);
+  return <div>
+    {count}
+  </div>
 }
+
+function Buttons(){
+  const { setCount } = useContext(CountContext);
+  return <div>
+    <button onClick={()=>{
+      setCount(c => c + 1);
+    }}>
+      Increase
+    </button>
+    <button onClick={()=>{
+      setCount(c => c - 1);
+    }}>
+      Decrease
+    </button>
+  </div>
+}
+
+// function TopBar(){
+
+//   const navigate = useNavigate();
+
+//   return (
+//     <div>
+
+//       <button onClick={()=>{
+//         navigate('/');
+//       }}>
+//         Landing
+//       </button>
+
+//       <button onClick={()=>{
+//         navigate('/dashboard');
+//       }}>
+//         Dashboard
+//       </button>
+
+//     </div>
+//   )
+// }
 
 export default App
