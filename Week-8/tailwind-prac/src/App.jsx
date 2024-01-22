20079844dad06b097c9d3dd873ca54410e8abc44
gautam1228 <gautamsingh12122003@gmail.com>
2024-01-22 22:47:46 +0530

    Modifications.

--- Week-8/tailwind-prac/src/App.jsx
+++ Week-8/tailwind-prac/src/App.jsx
@@ -49,10 +49,11 @@ function App() {
 
       <div><br></br></div>
 
-      <div className='grid grid-cols-1 md:grid grid-cols-3'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
         <div className='bg-red-500'>Hi</div>
         <div className='bg-green-500'>Hi</div>
         <div className='bg-yellow-500'>Hi</div>
+        <div className='bg-blue-500'>Hi</div>
       </div>
 
     </>
