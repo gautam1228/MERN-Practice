2fdf2c18b79f9a92a778a1fea1fb5df46486f921
gautam1228 <gautamsingh12122003@gmail.com>
2024-03-24 21:57:13 +0530

    Added a react app to the monorepo as well as learn about dependencies in monorepos, also learn to add new components.

---
+++ Week-16/week-16-2/apps/web/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import { Admin } from "@repo/ui/admin";
+
+export default function(){
+
+    return (
+        <div>
+            Hi from admin page !
+            <Admin/>
+        </div>
+    )
+
+}
