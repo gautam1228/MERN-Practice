4d64c126823d786d799ab0b2a92db2913bdd46a2
gautam1228 <gautamsingh12122003@gmail.com>
2024-03-25 01:09:37 +0530

    Added esbuild to resolve the typescript error I was getting when exporting from the common module in packages.

--- Week-16/week-16-2/apps/backend/src/index.ts
+++ Week-16/week-16-2/apps/backend/src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import { BACKEND_URL } from "@repo/common/config";
 
 const app = express();
 
+console.log(BACKEND_URL);
 app.get("/", (req, res) => {
-
     res.status(200).json({
         msg : "Healthy server !"
     });
