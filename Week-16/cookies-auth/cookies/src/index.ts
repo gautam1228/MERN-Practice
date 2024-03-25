2f42b714d14279aae000c878d5a0a3d1cc3d4542
gautam1228 <gautamsingh12122003@gmail.com>
2024-03-26 01:30:59 +0530

    Learn about cookies using jwt.

---
+++ Week-16/cookies-auth/cookies/src/index.ts
@@ -0,0 +1,47 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import path from 'path';
+
+const JWT_SECRET = 'mysecret';
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:5173"
+}));
+
+
+app.post("/signin", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    // do db validations etc
+    const token = jwt.sign({
+        id: 1
+    }, JWT_SECRET);
+
+    res.cookie("token", token);
+    res.send("Logged in !");
+
+});
+
+app.get("/user", (req, res) => {
+
+    const token = req.cookies.token;
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    // Get email of the user from the db.
+    res.send({
+        userId : decoded.id
+    });
+
+});
+
+app.post("/logout", (req, res) => {
+    res.json({
+        msg : "Logged Out !"
+    })
+});
+
+app.listen(3000);
