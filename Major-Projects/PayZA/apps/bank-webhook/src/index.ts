2b1f29c9e621596fe11e29d67db18e20edc7cffd
gautam1228 <gautamsingh12122003@gmail.com>
2024-06-17 13:25:03 +0530

    Updated: db schema, added txns and balances. Update: added backend webhook handler.

--- Major-Projects/PayZA/apps/bank-webhook/src/index.ts
+++ Major-Projects/PayZA/apps/bank-webhook/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import db from "@repo/db/client";
 
 const app = express();
 
-app.post("/dummyBankWebHook", (req, res) => {
+app.post("/dummyBankWebHook", async (req, res) => {
 
     const paymentInformation = {
         token: req.body.token,
@@ -10,6 +11,54 @@ app.post("/dummyBankWebHook", (req, res) => {
         amount : req.body.amount
     };
 
+
+    try{
+        await db.$transaction([
+    
+            db.balance.update({
+                where : {
+                    userId: Number(paymentInformation.userId)
+                },
+                data : {
+                    amount : {
+                        increment: Number(paymentInformation.amount)
+                    }
+                }
+            }),
+            db.onRampTransaction.update({
+                where : {
+                    token: paymentInformation.token
+                },
+                data : {
+                    status : "Success"
+                }
+            })
+    
+        ]);
+    }
+    catch(e){
+        console.error(e);
+        res.status(411).json({
+            message : "Error while processing webhook."
+        });
+
+        await db.onRampTransaction.update({
+            where : {
+                token: paymentInformation.token
+            },
+            data : {
+                status : "Failure"
+            }
+        });
+
+    }
+
+
+    res.status(200).json({
+        message : "Captured !"
+    });
+
+
 });
 
-app.listen(3002);
+app.listen(3003);
