e701b57fdc6b89ed04b999e26a5d39e42f1623fc
Turbobot <turbobot@vercel.com>
2024-08-10 22:51:10 +0530

    feat(create-turbo): apply official-starter transform

--- Major-Projects/CashIT/apps/user-app/app/lib/auth.ts
+++ Major-Projects/CashIT/apps/user-app/app/lib/auth.ts
@@ -1,5 +1,5 @@
 import db from "@repo/db/client";
-import CredentialsProvider from "next-auth/providers/credentials"
+import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 
 export const authOptions = {
