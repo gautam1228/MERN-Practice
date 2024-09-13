08ea22bbfc5a2ac8272ce19eec9a796165ab2f81
gautam1228 <gautamsingh12122003@gmail.com>
2024-09-14 02:00:06 +0530

    Learned Array Basics

---
+++ JS/02-basics/02_arrays.js
@@ -0,0 +1,36 @@
+
+const marvel_heros = ["IronMan", "CaptainAmerica", "Thor"];
+const dc_heros = ["SuperMan", "Flash", "Batman"];
+
+
+// marvel_heros.push(dc_heros); // Will insert Array, within the Array
+// console.log(marvel_heros);
+
+// const allHeros = marvel_heros.concat(dc_heros); //  Creates a new Array with the elements of both the Arrays
+// console.log(allHeros);
+
+//                                    OR
+
+// const allHeros = [...marvel_heros, ...dc_heros];
+// console.log(allHeros);
+
+
+// const anotherArray = [1, 2, 3, [4, 5, 6], 7, [ 8, 9, [10, 11]]];
+// const usableArray = anotherArray.flat(2);
+// console.log(usableArray);
+
+
+// console.log(Array.isArray("Gautam"));
+// console.log(Array.from("Gautam"));
+// console.log(Array.from({
+//     name : "Gautam"
+// })); // Gives an Empty Array ( As it doesn't whether to create an array of keys or values )
+
+
+
+// const num1 = 123;
+// const num2 = 223;
+// const num3 = 323;
+
+// console.log(Array.of(num1, num2, num3));
+
