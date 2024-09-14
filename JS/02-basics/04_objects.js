b950dbba33eac706e2486a5081702f9a70f9bd8d
gautam1228 <gautamsingh12122003@gmail.com>
2024-09-15 04:10:03 +0530

    Added some object concepts

--- JS/02-basics/04_objects.js
+++ JS/02-basics/04_objects.js
@@ -8,10 +8,25 @@ const obj2 = { 3: "c", 4: "d" };
 // const obj3 = Object.assign({}, obj1, obj2); // Syntax : Object.assign(target, source)
 const obj3 = {...obj1, ...obj2};
 
-console.log(obj3);
+// console.log(obj3);
 
-console.log(Object.keys(obj1));
-console.log(Object.keys(obj1));
-console.log(Object.entries(obj1));
+// console.log(Object.keys(obj1));
+// console.log(Object.keys(obj1));
+// console.log(Object.entries(obj1));
 
-console.log(obj1.hasOwnProperty("1")); // To check if property exists
+// console.log(obj1.hasOwnProperty("1")); // To check if property exists
+
+
+// Object De-Structuring
+
+const course = {
+    name : "Graphic Design",
+    price : "999",
+    courseInstructor : "Gautam Singh"
+};
+
+// const { courseInstructor } = course;
+// const { courseInstructor: instructor } = course;
+// const { name, price } = course;
+
+// console.log(course);
