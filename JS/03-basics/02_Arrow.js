901b5148af20bd3d6494df0997eada16e6aacacd
gautam1228 <gautamsingh12122003@gmail.com>
2024-09-18 16:32:34 +0530

    Prettier changed some stuff.

--- JS/03-basics/02_Arrow.js
+++ JS/03-basics/02_Arrow.js
@@ -1,10 +1,9 @@
-
 const user = {
-    username : "Gautam",
-    price : 999,
-    welcomeMessage : function(){
+    username: "Gautam",
+    price: 999,
+    welcomeMessage: function () {
         console.log(`Hi ${this.username}, welcome to the website !`);
-    }
+    },
 };
 
 // user.welcomeMessage();
@@ -15,17 +14,15 @@ const user = {
 
 // Earlier JS could only be executed inside the browser, now, this engine which is used to run JS is standalone, by the name of ( Node, Bun, Dino )
 
-function randm(){
+function randm() {
     const username = "random123";
     console.log(this); // Outputs some complex object
     console.log(this.username); // Gives output : undefined
 }
 
-
-
 const newRandm = () => {
     console.log(this);
-}
+};
 
 // newRandm(); // Outputs : {}
 
@@ -33,19 +30,16 @@ const addTwo = (num1, num2) => {
     return num1 + num2;
 };
 
-console.log(addTwo(3,5));
-
-
+console.log(addTwo(3, 5));
 
 // Implicit return function
 
 const addTwoNew = (num1, num2) => num1 + num2;
 //                OR
-const addTwoNew1 = (num1, num2) => ( num1 + num2 );
-
+const addTwoNew1 = (num1, num2) => num1 + num2;
 
 // To return an object
 
 // const returnObj = () => {username:'Gautam'} // Will give output : undefined
 
-const returnObj = () => ({username:'Gautam'}) // Correct way to return an object
+const returnObj = () => ({ username: "Gautam" }); // Correct way to return an object
