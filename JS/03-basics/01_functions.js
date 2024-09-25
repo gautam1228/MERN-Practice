73a0a78602d73c8cb880cf502bf8bfc85f6a6267
gautam1228 <gautamsingh12122003@gmail.com>
2024-09-25 23:31:16 +0530

    Learn about functions and loops.

--- JS/03-basics/01_functions.js
+++ JS/03-basics/01_functions.js
@@ -1,24 +1,50 @@
-
-function calculateCartPrice(...num1){         // Rest Operator
-    return num1
+function calculateCartPrice(...num1) {
+    // Rest Operator
+    return num1;
 }
-function calculateCartPrice1(val1, val2, ...num1){
-    return num1
+function calculateCartPrice1(val1, val2, ...num1) {
+    return num1;
 }
 
 // console.log(calculateCartPrice(200, 300, 400, 100, 500));
 // console.log(calculateCartPrice1(200, 300, 400, 100, 500));
 
+// console.log(addOne(5));
 
-console.log(addOne(5));
-
-function addOne(num){
-    return num + 1
+function addOne(num) {
+    return num + 1;
 }
-
 
 // addTwo(5); // Gives an error.
 
-const addTwo = function(num){
-    return num + 2
+const addTwo = function (num) {
+    return num + 2;
+};
+
+// Higher Order Function
+
+// function makeTea(typeOfTea) {
+//     return `Maketea : ${typeOfTea}`;
+// }
+
+// // This is a higher order function
+// function processTeaOrder(teaFunction) {
+//     return teaFunction("earl grey");
+// }
+
+// let order = processTeaOrder(makeTea);
+// console.log(order);
+
+// Another Higher Order Function
+
+function createTeaMaker(name) {
+    return function (teaType) {
+        return `Making ${teaType} for ${name} !`;
+    };
 }
+
+let teaMaker = createTeaMaker("Gautam");
+console.log(teaMaker("green tea"));
+
+// This shows us that inside functions, if we define functions, we have access to all the variables of the parent function, which is contradicting to how JS works.
+// This behaviour is called ' closure ' in JS
--- JS/04-Control-flow/02_loops.js
+++ JS/04-Control-flow/02_loops.js
@@ -28,7 +28,7 @@
 
 // console.log(visitedCities);
 
-// Challenge - 3
+// Challenge - 3            [ For - Of Loop ]
 
 // let arr = [1, 2, 3, 4, 5];
 // let smallNumbers = [];
@@ -42,16 +42,90 @@
 
 // console.log(smallNumbers);
 
-// Challenge - 4
+// Challenge - 4            [ For - Of Loop ]
 
-let teas = ["chai", "green tea", "herbal tea", "black tea"];
-let preferredTeas = [];
-for (const element of teas) {
-    if (element === "herbal tea") {
-        continue;
-    } else {
-        preferredTeas.push(element);
-    }
-}
+// let teas = ["chai", "green tea", "herbal tea", "black tea"];
+// let preferredTeas = [];
+// for (const element of teas) {
+//     if (element === "herbal tea") {
+//         continue;
+//     } else {
+//         preferredTeas.push(element);
+//     }
+// }
 
-console.log(preferredTeas);
+// console.log(preferredTeas);
+
+// Challenge - 5          [ For - In Loop ]
+
+// let citiesPopulation = {
+//     London: 8900000,
+//     "New York": 8400000,
+//     Paris: 2200000,
+//     Berlin: 3500000,
+// };
+
+// let citiesPopulationNew = {};
+
+// for (const city in citiesPopulation) {
+//     if (city === "Berlin") break;
+//     citiesPopulationNew[city] = citiesPopulation[city];
+// }
+
+// console.log(citiesPopulationNew);
+
+// Challenge - 6        [ For - In Loop ]
+
+// let worldCities = {
+//     Sydney: 5000000,
+//     Tokyo: 9000000,
+//     Berlin: 3500000,
+//     Paris: 2200000,
+// };
+
+// let largeCities = {};
+
+// for (const city in worldCities) {
+//     if (worldCities[city] < 3000000) continue;
+//     else largeCities[city] = worldCities[city];
+// }
+
+// console.log(largeCities);
+
+// Challenge - 7        [ For - Each Loop ]
+
+// let teas = ["earl grey", "green tea", "chai", "oolong tea"];
+
+// let availableTeas = [];
+
+// teas.forEach((tea) => {
+//     if (tea === "chai") return;
+//     else availableTeas.push(tea);
+// });
+
+// console.log(availableTeas);
+
+// Challenge - 8
+
+// let arr = [2, 5, 7, 9];
+// let doubledNumbers = [];
+
+// arr.forEach((num) => {
+//     if (num === 7) return;
+//     else doubledNumbers.push(num * 2);
+// });
+
+// console.log(doubledNumbers);
+
+// Challenge - 9
+
+// let teas = ["chai", "green tea", "jasmine tea", "black tea", "herbal tea"];
+
+// let shortTeas = [];
+
+// for (const tea of teas) {
+//     if (tea.length > 10) break;
+//     else shortTeas.push(tea);
+// }
+
+// console.log(shortTeas);
