b015e066a7cd9c153d56c1ee16d37e7a4d2e4ed7
gautam1228 <gautamsingh12122003@gmail.com>
2024-10-29 02:38:26 +0530

    Learned some tailwind with react

---
+++ React/01-tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <>
+            <div className="flex justify-center h-screen">
+                <div className="flex flex-col justify-center h-full">
+                    <h1 className="text-2xl bg-green-200 p-5 rounded-xl">
+                        Vite and Tailwind APP
+                    </h1>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ React/01-tailwindprops/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Card() {
+    return (
+        <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate,
+            et.
+        </div>
+    );
+}
+
+export default Card;
