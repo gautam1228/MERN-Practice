435e244374ef1c7ef1c4c2e440ec3b5a3b2f5b70
gautam1228 <gautamsingh12122003@gmail.com>
2024-11-08 05:54:22 +0530

    Added an error page for page not found error

--- React/05-routing/src/App.jsx
+++ React/05-routing/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/register" element={<RegisterPage />} />
+                    <Route path="*" element={<ErrorPage />} />
                 </Routes>
             </BrowserRouter>
         </>
