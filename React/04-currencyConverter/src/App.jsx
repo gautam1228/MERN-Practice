fae955bbedcc7ae6642bd377e4e62fb180fc2a49
gautam1228 <gautamsingh12122003@gmail.com>
2024-12-28 15:40:49 +0530

    Adding everything for now

---
+++ React/04-currencyConverter/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+    return (
+        <>
+            <div
+                className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+                style={{
+                    backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`,
+                }}
+            >
+                <h1>Test for tailwind</h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
--- React/07-recoil/src/store/atoms/counter.js
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const counterAtom = atom({
-    key: "counter",
-    default: 0
-});
